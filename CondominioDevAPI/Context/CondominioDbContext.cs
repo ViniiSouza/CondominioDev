846b1d152880728cccd9ba1eab63000c301cd4c1
Vinícius <vinigsouza.bnu@gmail.com>
2022-05-24 22:10:29 -0300

    Criação e configuração do contexto e migration inicial

---
+++ CondominioDevAPI/Context/CondominioDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CondominioDevAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CondominioDevAPI.Context
+{
+    public class CondominioDbContext : DbContext
+    {
+        public DbSet<Habitante> Habitante { get; set; }
+
+        public CondominioDbContext(DbContextOptions options) : base(options) {}
+    }
+}
