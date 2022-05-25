9da6ad9cebe00f64dc89044946a15f12b4d63010
Vinícius <vinigsouza.bnu@gmail.com>
2022-05-24 22:00:02 -0300

    Criação da classe habitante

---
+++ CondominioDevAPI/Models/Habitante.cs
@@ -0,0 +1,17 @@
+﻿namespace CondominioDevAPI.Models
+{
+    public class Habitante
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public float Renda { get; set; }
+
+        public string CPF { get; set; }
+    }
+}
