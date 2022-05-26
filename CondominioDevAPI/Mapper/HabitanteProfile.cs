e23442c4e181eaa435bbb92a58fac527048fe376
Vinícius <vinigsouza.bnu@gmail.com>
2022-05-25 21:06:45 -0300

    Criação do mapeamento de DTO e modelo

---
+++ CondominioDevAPI/Mapper/HabitanteProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CondominioDevAPI.DTOs;
+using CondominioDevAPI.Models;
+
+namespace CondominioDevAPI.Mapper
+{
+    public class HabitanteProfile : Profile
+    {
+        public HabitanteProfile()
+        {
+            CreateMap<HabitantePostDTO, Habitante>()
+                .ForMember(dest => dest.CPF, opt => opt.MapFrom(src => src.CPF.Replace(".", "").Replace("-", "")))
+                .ForMember(dest => dest.DataNascimento, opt => opt.MapFrom(src => DateTime.ParseExact(src.DataNascimento, "dd/MM/yyyy", null)));
+        }
+    }
+}
